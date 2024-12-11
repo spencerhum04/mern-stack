d59b119a383cd96cce724e75092edeaf9b2e13d0
spencerhum04 <spencerhum04@gmail.com>
2024-12-10 23:50:52 -0500

    feat: form

---
+++ frontend/src/components/form/form.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const Form = () => {
+    const [title, setTitle] = useState('');
+    const [load, setLoad] = useState('');
+    const [reps, setReps] = useState('');
+    const [error, setError] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        setSubmitted(true);
+
+        const workout = {title, load, reps }
+
+        try {
+            const response = await fetch('/api/workouts', {
+                method: 'POST',
+                body: JSON.stringify(workout),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const json = await response.json();
+
+            if (!response.ok) {
+                setError(json.error || 'An unexpected error occurred');
+                return;
+            }
+
+            setError(null);
+            setTitle('');
+            setLoad('');
+            setReps('');
+            console.log('New workout added:', json);
+        } catch (err) {
+            // setError('Failed to submit the workout. Please try again.');
+        }
+    }
+
+    return (
+        <form className="border border-black p-2 bg-white flex flex-col" onSubmit={handleSubmit}>
+            <div>Add a new workout</div>
+            <label>Exercise title:</label>
+            <input className="border border-black px-2.5 py-2 focus:outline-none" type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
+            <label>Load (kg):</label>
+            <input className="border border-black px-2.5 py-2 focus:outline-none" type="number" onChange={(e) => setLoad(e.target.value)} value={load} />
+            <label>Reps:</label>
+            <input className="border border-black px-2.5 py-2 focus:outline-none" type="number" onChange={(e) => setReps(e.target.value)} value={reps} />
+            <button className="border border-black bg-slate-100 hover:bg-slate-200 mt-2.5">Add Workout</button>
+            {submitted && error &&
+                <div>{error}</div>
+            }
+            {submitted && !error &&
+                <div>New workout added</div>
+            }
+        </form>
+    )
+}
+
+export default Form;
